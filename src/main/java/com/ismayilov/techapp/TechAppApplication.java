22f934b43dd225e4e0cd25320695167f423eb2ec
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2025-11-04 23:53:04 +0400

    firs commit

---
+++ src/main/java/com/ismayilov/techapp/TechAppApplication.java
@@ -0,0 +1,13 @@
+package com.ismayilov.techapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechAppApplication.class, args);
+    }
+
+}
