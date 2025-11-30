9e230880ab2678de6c784f0a200f775ccc4dde5e
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2025-11-30 17:42:44 +0400

    work on fourth service

--- src/main/java/com/ismayilov/techapp/config/ApplicationConfig.java
+++ src/main/java/com/ismayilov/techapp/config/ApplicationConfig.java
@@ -1,17 +1,38 @@
 package com.ismayilov.techapp.config;
 
+import lombok.Getter;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.slf4j.Logger;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class ApplicationConfig {
 
+    @Getter
+    private static String urlMB;
+
     @Bean
     public Logger logger() {
         return LoggerFactory.getLogger(getClass().getName());
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Value("${URL_MB}")
+    public void setUrlMB(String urlMB) {
+        ApplicationConfig.urlMB = String.format(
+                urlMB, LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+        );
     }
 
 }
---
+++ src/main/java/com/ismayilov/techapp/restclient/CbarRestClient.java
@@ -0,0 +1,10 @@
+package com.ismayilov.techapp.restclient;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE )
+public class CbarRestClient {
+}
